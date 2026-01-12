17717c28b63cad33729af7b4ddb96fd2983aa431
kirill <k.ovchinnikov06@gmail.com>
2026-01-12 16:57:38 +0300

    Добавил поиск(v0.6.0)

--- src/data.js
+++ src/data.js
@@ -1,24 +1,35 @@
 export const users = [
     {
-        id: 1959,
+        id: 0,
         role: 'Админ',
         name: 'Александр',
         login: 'semelDesigner',
-        position: 'BackEnd разработчик',
+        position: 'FrontEnd разработчик',
         contacts: '@test',
         email: 'test@test.ru',
         phone: '+7-9999999999',
         city: 'Нижний Новгород'
     },
     {
-        id: 1960,
+        id: 1,
         role: 'Разработчик',
         name: 'Василий',
         login: 'vasyaDev',
-        position: 'FrontEnd Разработчик',
+        position: 'BackEnd разработчик',
         contacts: '@vasya',
         email: 'vasya@test.ru',
         phone: '+7-8888888888',
         city: 'Москва'
     },
+    {
+        id: 2,
+        role: 'Тимлид',
+        name: 'Ольга',
+        login: 'olgaManager',
+        position: 'FullStack разработчик',
+        contacts: '@olga',
+        email: 'olga@test.ru',
+        phone: '+7-7777777777',
+        city: 'Санкт-Петербург'
+    }
 ];
